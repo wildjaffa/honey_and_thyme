0d40547918826b3607ec4807f249106d98863a1b
wildjaffa <sungujdj@gmail.com>
2024-04-12 20:48:08 -0500

    added initial loader

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,7 +7,6 @@ import 'home/home.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
