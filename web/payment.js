aad65fb75df8d39eb6665168c032b713e5150f11
josh-jensen <josh.jensen@fortyau.com>
2025-03-03 20:00:33 -0600

    remove paypal-popup

--- web/payment.js
+++ web/payment.js
@@ -1,42 +1,39 @@
 globalThis.openPayPalWindow = (captureAmount, baseUrl, orderId) => {
-  const paypalWindow = window.open(
-    "/payment.html",
-    "Honey + Thyme Payment",
-    "width=600,height=800"
-  );
-
-  paypalWindow.onload = function () {
-    paypalWindow.paypal
-      .Buttons({
-        style: {
-          shape: "rect",
-          layout: "vertical",
-          color: "gold",
-          label: "paypal",
-        },
-        async createOrder() {
-          try {
-            return orderId;
-          } catch (error) {
-            opener.globalThis.onerror(
-              "There was a problem processing your payment, you will not be charged"
-            );
-            console.error(error);
-          }
-        },
-        async onApprove(data, actions) {
-          globalThis.onApprove(data.orderID);
-          setTimeout(() => {
-            paypalWindow.close();
-          }, 20);
-        },
-      })
-      .render("#paypal-button-container");
-
-    const windowChecker = setInterval(() => {
-      if (!paypalWindow.closed) return;
-      globalThis.onPayPalWindowClose();
-      clearInterval(windowChecker);
-    }, 1000);
+  const paypalForm = document.getElementById("paypal-payment-form");
+  paypalForm.style.display = "block";
+  window.paypal
+    .Buttons({
+      style: {
+        shape: "rect",
+        layout: "vertical",
+        color: "gold",
+        label: "paypal",
+      },
+      async createOrder() {
+        try {
+          return orderId;
+        } catch (error) {
+          globalThis.onerror(
+            "There was a problem processing your payment, you will not be charged"
+          );
+          console.error(error);
+        }
+      },
+      async onApprove(data, actions) {
+        paypalForm.style.display = "none";
+        globalThis.onApprove(data.orderID);
+      },
+    })
+    .render("#paypal-button-container");
+  const closeButton = document.getElementById("close-paypal");
+  closeButton.onclick = () => {
+    paypalForm.style.display = "none";
+    globalThis.onPayPalWindowClose();
+    document.getElementById("paypal-button-container").remove();
+    const paypalButtons = document.createElement("paypal-button");
+    paypalButtons.id = "paypal-button-container";
+    document
+      .getElementById("paypal-payment-form-inner")
+      .appendChild(paypalButtons);
   };
 };
