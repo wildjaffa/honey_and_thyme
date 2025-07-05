b3e9292c982e40d6c296944eafc549f7f141e012
josh-jensen <josh.jensen@fortyau.com>
2025-07-05 14:16:53 -0500

    fix flutter localizations

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'localization/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:honey_and_thyme/src/admin/album/album_list.dart';
